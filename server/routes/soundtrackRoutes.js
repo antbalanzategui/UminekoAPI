fcccd5e48ca2bbf856310e050e1cb441860b3f48
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-05-18 20:57:46 -0400

    Commented Recently Added Code

--- server/routes/soundtrackRoutes.js
+++ server/routes/soundtrackRoutes.js
@@ -7,6 +7,8 @@ validateQuery = queryValidator.validateQuery;
 const paramConverter = require('../utils/paramConverter');
 const convertToInt = paramConverter.convertToInt;
 
+// This establishes the data which we will get when we call soundtrack/id=? 
+// we will fetch data from the database of the soundtrack where id is = ?
 router.get('/id=:id?', async (req, res, next) => {
     if (!req.params.id) {
         return res.status(400).json({ error: 'Missing "id" parameter. Please include an "id" parameter in the request URL.' })
@@ -27,6 +29,8 @@ router.get('/id=:id?', async (req, res, next) => {
       }
   });
 
+// This establishes the data which we will get when we call soundtrack/title=?
+// we will fetch data from the database of the soundtrack where title partially matches the ?
 router.get('/title=:title?', async (req, res, next) => {
     if (!req.params.title) {
         return res.status(400).json({ error: 'Missing "title" parameter. Please include an "title" parameter in the request URL.' })
@@ -46,7 +50,8 @@ router.get('/title=:title?', async (req, res, next) => {
         res.sendStatus(500);
       }
 });
-
+// This establishes the data which we will get when we call soundtrack/composer=?
+// we will fetch data from the database of the soundtrack where composer partially matches the ?
 router.get('/composer=:composer?', async (req, res, next) => {
     if (!req.params.composer) {
         return res.status(400).json({ error: 'Missing "composer" parameter. Please include an "composer" parameter in the request URL.' })
@@ -66,7 +71,8 @@ router.get('/composer=:composer?', async (req, res, next) => {
         res.sendStatus(500);
       }
 });
-
+// This establishes the data which we will get when we call soundtrack/episode=?
+// we will fetch data from the database of the soundtrack where episode = "Episode " + ?
 router.get('/episode=:episode?', async (req, res, next) => {
     if (!req.params.episode) {
         return res.status(400).json({ error: 'Missing "episode" parameter. Please include an "episode" parameter in the request URL.' })
@@ -90,7 +96,9 @@ router.get('/episode=:episode?', async (req, res, next) => {
       }
   });
 
-  router.get('/', convertToInt, async (req, res, next) => {
+// This combines all the previous queries methods into one, in which
+// allows for users to simultaneously query multiple parameters
+router.get('/', convertToInt, async (req, res, next) => {
 
     const errors = validateQuery(req.query, soundTrackSchema);
 
--- server/utils/paramConverter.js
+++ server/utils/paramConverter.js
@@ -1,3 +1,6 @@
+// This function is a used to convert certain possible parameters within a query to an integer
+// essentially, this is necessary occasionally as we need to evaluate whether it is within a particular
+// range of numbers, positive, etc...
 const convertToInt = (req, res, next) => {
     if (req.query.id) {
       req.query.id = parseInt(req.query.id);
