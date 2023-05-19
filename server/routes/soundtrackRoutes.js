e0d9952c1e5edc42cef9eea288bf4ad37bd8dfa3
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-05-19 15:05:16 -0400

    Accomdated New Query Handler, Same Functionality

--- server/routes/soundtrackRoutes.js
+++ server/routes/soundtrackRoutes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/soundtrackDB');
 const queryValidator = require('../utils/queryValidator');
-soundTrackSchema = queryValidator.soundTrackSchema;
-validateQuery = queryValidator.validateQuery;
+const soundTrackSchema = queryValidator.soundTrackSchema;
 const paramConverter = require('../utils/paramConverter');
 const convertToInt = paramConverter.convertToInt;
 const queryHandler = require('../utils/queryHandler');
@@ -16,7 +15,7 @@ router.get('/id=:id?', async (req, res, next) => {
         return res.status(400).json({ error: 'Missing "id" parameter. Please include an "id" parameter in the request URL.' })
       }
       req.params.id = parseInt(req.params.id);
-      await handleQuery(req, res, next, db.soundtrackById.bind(null, req.params.id), req.params);
+      await handleQuery(req, res, next, db.soundtrackById.bind(null, req.params.id), req.params, soundTrackSchema);
   });
 
 // This establishes the data which we will get when we call soundtrack/title=?
@@ -25,7 +24,7 @@ router.get('/title=:title?', async (req, res, next) => {
     if (!req.params.title) {
         return res.status(400).json({ error: 'Missing "title" parameter. Please include an "title" parameter in the request URL.' })
       }
-      await handleQuery(req, res, next, db.soundtrackByTitle.bind(null, req.params.title), req.params);
+      await handleQuery(req, res, next, db.soundtrackByTitle.bind(null, req.params.title), req.params, soundTrackSchema);
 });
 // This establishes the data which we will get when we call soundtrack/composer=?
 // we will fetch data from the database of the soundtrack where composer partially matches the ?
@@ -33,7 +32,7 @@ router.get('/composer=:composer?', async (req, res, next) => {
     if (!req.params.composer) {
         return res.status(400).json({ error: 'Missing "composer" parameter. Please include an "composer" parameter in the request URL.' })
       }
-      await handleQuery(req, res, next, db.soundtrackByComposer.bind(null, req.params.composer), req.params);
+      await handleQuery(req, res, next, db.soundtrackByComposer.bind(null, req.params.composer), req.params, soundTrackSchema);
 });
 // This establishes the data which we will get when we call soundtrack/episode=?
 // we will fetch data from the database of the soundtrack where episode = "Episode " + ?
@@ -41,24 +40,16 @@ router.get('/episode=:episode?', async (req, res, next) => {
     if (!req.params.episode) {
         return res.status(400).json({ error: 'Missing "episode" parameter. Please include an "episode" parameter in the request URL.' })
       }
-      const keptString = req.params.episode;
       req.params.episode = parseInt(req.params.episode);
-      const queryString = "Episode " + keptString;
-      await handleQuery(req, res, next, db.soundtrackByEpisode.bind(null, queryString), req.params);
+      await handleQuery(req, res, next, db.soundtrackByEpisode.bind(null, req.params.episode), req.params, soundTrackSchema);
   });
 
 // This combines all the previous queries methods into one, in which
 // allows for users to simultaneously query multiple parameters
 router.get('/', convertToInt, async (req, res, next) => {
-
-    const errors = validateQuery(req.query, soundTrackSchema);
-
-    if (errors.length > 0) {
-      res.status(400).json({ errors });
-      return;
-    }
-  
-    await handleQuery(req, res, next, db.soundtrackByQuery, req.query);
+    await handleQuery(req, res, next, db.soundtrackByQuery, req.query, soundTrackSchema);
   });
 
   module.exports = router;
+
+  //@popular @category:"themes" 
