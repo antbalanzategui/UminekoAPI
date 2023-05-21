8b745af183188690c1a9009fd0874ffeca96f906
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-05-21 16:36:41 -0400

    UI Updates

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,20 +1,43 @@
 import { Outlet, Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MarkGithubIcon } from '@primer/octicons-react';
-import { Sun } from 'react-feather';
-import { Moon } from 'react-feather';
-import './Header.css'
+import { Sun, Moon } from 'react-feather';
+import './styles/Header.css';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [isLightMode, setIsLightMode] = useState(false);
+
+  useEffect(() => {
+    const storedPreference = localStorage.getItem('lightMode');
+    if (storedPreference !== null) {
+      setIsLightMode(storedPreference === 'true');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLightMode) {
+      document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    localStorage.setItem('lightMode', isLightMode.toString());
+  }, [isLightMode]);
+
+  const toggleLightMode = () => {
+    setIsLightMode(prevMode => !prevMode);
+  };
+
   return (
     <>
-      <nav>
-        <ul className = 'navbar-list'>
+      <nav className = "sticky-navbar">
+        <ul className='navbar-list'>
           <li>
-          <img src={process.env.PUBLIC_URL + '/Umineko_One-Winged_Eagle.png'} alt="Umineko Logo" className='logo'/>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
+            <Link to="/">
+              <div className="HomeDiv">
+                <img src={process.env.PUBLIC_URL + '/Umineko_One-Winged_Eagle.png'} alt="Umineko Logo" className='logo' />
+                <span className="UmiSpan">UminekoAPI</span>
+              </div>
+            </Link>
           </li>
           <li>
             <Link to="/documentation">Documentation</Link>
@@ -23,19 +46,23 @@ const Header = () => {
             <Link to="/projects">Projects</Link>
           </li>
           <li>
-          <Sun size={32} />
-          {/* Sun icon */}
-        </li>
+            <button onClick={toggleLightMode}>
+              {isLightMode ? <Moon size={32} /> : <Sun size={32} />}
+              {/* Moon/Sun icon */}
+            </button>
+          </li>
           <li>
-          <MarkGithubIcon size={32} />
-          {/* GitHub icon */}
+            <a href="https://github.com/antbalanzategui/UminekoAPI" target="_blank" rel="noopener noreferrer">
+              <MarkGithubIcon size={32} />
+              {/* GitHub icon */}
+            </a>
           </li>
         </ul>
       </nav>
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Header;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,20 @@
 import Footer from './Footer';
+import './styles/Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is the Home Page</h1>
+    <div className = "IntroDiv">
+      <h1>
+        A User-Friendly API For Visual Novel Series
+      </h1>
+      <span>
+        <h1>
+          Umineko When They Cry
+          </h1>
+        </span>
+      <span>
+        <h3>Access Information Regarding the Visual Novel's SoundTrack, Characters, Computer Graphics, and Quotes</h3>
+      </span>
       <Footer />
     </div>
   );
