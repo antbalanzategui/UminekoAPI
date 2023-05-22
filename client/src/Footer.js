2b15f03b35a39f788f3c0f7871a294b9e439772c
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-05-22 02:35:27 -0400

    Created LayOut For HomePage

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { MarkGithubIcon } from '@primer/octicons-react';
+import './styles/Footer.css';
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return <footer className="footer">
+  <div className="footer-content">
+    <div className="footer-links">
+      <a href="/contact">Contact</a>
+    </div>
+    <div className="footer-bottom">
+    <p>© {new Date().getFullYear()} UminekoAPI. All rights reserved.</p>
+  </div>
+    <div className="footer-social">
+      <a href="https://github.com/antbalanzategui/UminekoAPI" target="_blank" rel="noopener noreferrer">
+      <MarkGithubIcon size={32} />
+      </a>
+    </div>
+  </div>
+</footer>;
 }
 
 export default Footer;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,21 +1,109 @@
 import Footer from './Footer';
 import './styles/Home.css';
+import {Star, Users, Headphones, Calendar,  Image, MessageCircle} from 'react-feather';
+import { Card, CardContent, Typography} from '@mui/material';
+import { FaReddit } from 'react-icons/fa';
 
 const Home = () => {
   return (
-    <div className = "IntroDiv">
-      <h1>
-        A User-Friendly API For Visual Novel Series
-      </h1>
-      <span>
+    <div>
+    <div className="page-container">
+      <div className="IntroDiv">
         <h1>
-          Umineko When They Cry
+          A User-Friendly API For Visual Novel Series
+        </h1>
+        <span>
+          <h1 className="UminekoName">
+            Umineko When They Cry
           </h1>
         </span>
-      <span>
-        <h3>Access Information Regarding the Visual Novel's SoundTrack, Characters, Computer Graphics, and Quotes</h3>
-      </span>
-      <Footer />
+        <div className = "h3container">
+        <span>
+          <h3>Access Information Regarding the Visual Novel's</h3>
+        </span>
+        <span>
+          <h3>SoundTrack, Characters, Computer Graphics, Timeline, and Quotes</h3>
+        </span>
+        </div>
+      </div>
+      <div className ="FeatureSection">
+        <div className = "featureHeader">
+        <Star size={24}/>
+        <h1>Features</h1>
+        </div>
+        <div className="featuresGrid">
+      <Card className="gridItem">
+        <CardContent>
+          <Users size={24} />
+          <Typography variant="h5" component="h2">
+            Character Data
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Access to All Characters Within the Visual Novel
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className="gridItem">
+        <CardContent>
+          <Headphones size = {24}/>
+          <Typography variant="h5" component="h2">
+            SoundTrack Data
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Access to All SoundTracks Within the Visual Novel
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className="gridItem">
+        <CardContent>
+          <Calendar size = {24}/>
+          <Typography variant="h5" component="h2">
+            Timeline Data
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            An Indepth Timeline of All Events Within the Visual Novel
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className="gridItem">
+        <CardContent>
+          <Image size = {24}/>
+          <Typography variant="h5" component="h2">
+            Computer Graphics
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Access to Computer Graphics used throughout the Visual Novel
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className="gridItem">
+        <CardContent>
+          <MessageCircle size = {24}/>
+          <Typography variant="h5" component="h2">
+            Quotes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            An extensive list of Quotes used throughout the Visual Novel
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className="gridItem">
+        <CardContent>
+        <div className="card-icon">
+          <FaReddit className="inverted-icon" size={24} />
+        </div>
+          <Typography variant="h5" component="h2">
+            Reddit
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Incoming Feature...
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+      </div>
+    </div>
+    <Footer/>
     </div>
   );
 };
