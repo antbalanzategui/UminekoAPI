e72f63a950a93fb83ca66d3268dd5dec66ff50b6
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-05-23 01:58:20 -0400

    Added IconButton To GitHub Icon

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MarkGithubIcon } from '@primer/octicons-react';
 import './styles/Footer.css';
+import IconButton from '@mui/material/IconButton';
 
 function Footer() {
   return <footer className="footer">
@@ -12,9 +13,11 @@ function Footer() {
     <p>© {new Date().getFullYear()} UminekoAPI. All rights reserved.</p>
   </div>
     <div className="footer-social">
+      <IconButton>
       <a href="https://github.com/antbalanzategui/UminekoAPI" target="_blank" rel="noopener noreferrer">
       <MarkGithubIcon size={32} />
       </a>
+      </IconButton>
     </div>
   </div>
 </footer>;
