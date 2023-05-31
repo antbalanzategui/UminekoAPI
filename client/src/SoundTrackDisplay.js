6f8ffb8b7315dcd24b037982703336f61812d3ba
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-05-31 01:07:48 -0400

    Implemented new CG Showcase

--- client/src/SoundTrackDisplay.js
+++ client/src/SoundTrackDisplay.js
@@ -8,6 +8,7 @@ import './styles/SoundTrackDisplay.css'; // Import your custom CSS file for styl
 const SoundTrackDisplay = () => {
   const { id } = useParams();
   const [episodeData, setEpisodeData] = useState(null);
+  const [graphics, setGraphics] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -29,47 +30,97 @@ const SoundTrackDisplay = () => {
           console.error(error);
         }
       };
+
+      const fetchGraphics = async () => {
+        try {
+          const response = await fetch(`http://localhost:3001/api/images?episodeStart=${id}&episodeEnd=${id}&type=Graphic`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch graphics data');
+          }
+
+          const jsonData = await response.json();
+          setGraphics(jsonData);
+          console.log(jsonData);
+        } catch (error) {
+          console.error(error);
+        }
+      };
   
       fetchEpisodeData();
+      fetchGraphics();
     } else {
       // Handle invalid id (e.g., redirect to a not found page)
       navigate('/404'); // Replace '/404' with your desired not found page path
     }
   }, [id, navigate]);
 
-  if (!episodeData) {
+  if (!episodeData || !graphics) {
     return <div>Loading...</div>; // Show a loading state while fetching data
   }
-
-  console.log(episodeData);
-  console.log(episodeData[0]);
-  console.log(episodeData[0].composer);
-  console.log(episodeData[0].link);
   episodeData.length = 5;
+
+  console.log(id);
+  const intId = parseInt(id);
+  let graphicIndex;
+
+  if (intId === 1) {
+    graphicIndex = 0;
+  }
+  else if (intId === 2) {
+    graphicIndex = 0;
+  }
+  else if (intId === 3) {
+    graphicIndex = 1;
+  }
+  else if (intId === 4) {
+    graphicIndex = 4;
+  }
+  else if (intId === 5) {
+    graphicIndex = 1;
+  }
+  else if (intId === 6) {
+    graphicIndex = 2;
+  }
+  else if (intId === 7) {
+    graphicIndex = 2;
+  }
+  else if (intId === 8) {
+    graphicIndex = 4;
+  }
+  console.log(graphicIndex);
+  console.log(graphics);
+  console.log(graphics[graphicIndex].url);
+
+  
+  
 
   return (
     <div>
       <div className="showcaseHeader">
-      <h1>{`Episode: ${episodeData[0].episode}`}</h1>
-      <div>The Following is Simply a Way to Use the API to Decorate a WebPage using the SoundTrack Route</div>
+        <h1>{`Episode: ${episodeData[0].episode}`}</h1>
+        <div>The Following is Simply a Way to Use the API to Decorate a WebPage using the SoundTrack Route</div>
       </div>
       <div className="carouselContainer">
-      <Carousel showArrows={true} className="carousel">
-        {episodeData.map((episode) => (
-          <div key={episode.id}>
-            <iframe
-              title={episode.title}
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${episode.videoId}`}
-              allow="autoplay; encrypted-media"
-              allowFullScreen
-            ></iframe>
-            <h3>{episode.title}</h3>
-            <p>Composer: {episode.composer}</p>
-          </div>
-        ))}
-      </Carousel>
+        <Carousel showArrows={true} className="carousel">
+          {episodeData.map((episode) => (
+            <div key={episode.id}>
+              <iframe
+                title={episode.title}
+                src={`https://www.youtube.com/embed/${episode.videoId}`}
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+              ></iframe>
+              <h3>{episode.title}</h3>
+              <p>Composer: {episode.composer}</p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <div className="showcaseHeader">
+        For instance, if you wanted to display computer graphics from a particular episode using the Images route:
+      </div>
+      <div className="graphicContainer">
+      <img className ="graphic-image" src={graphics[graphicIndex].url}/>
       </div>
       <Footer />
     </div>
