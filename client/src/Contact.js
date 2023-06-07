43d4076d80da09a90984b4552d05dc8c278ef7d9
Antonio Balanzategui <antbalanzategui@vt.edu>
2023-06-07 00:28:18 -0400

    Test

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,9 +1,11 @@
 import Footer from './Footer';
+import Epitaph from './Epitaph';
 
 const Contact = () => {
   return (
     <div>
       <h1>This is Contact Page</h1>
+      <Epitaph/>
       <Footer />
     </div>
   );
